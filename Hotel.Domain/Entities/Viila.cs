58620cc983a0a76e04df10f8680855290d18749b
Mohamedzonkol <86891363+Mohamedzonkol@users.noreply.github.com>
2024-04-07 03:01:12 +0200

    And Some Crud OPreation of Villa Model (#1)

--- Hotel.Domain/Entities/Viila.cs
+++ Hotel.Domain/Entities/Viila.cs
@@ -1,13 +1,20 @@
-﻿namespace Hotel.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel.Domain.Entities
 {
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(100)]
         public required string Name { get; set; }
+        [MaxLength(1000)]
         public string? Description { get; set; }
+        [Display(Name = "Price Per Night"), Range(10, 100000)]
         public double Price { get; set; }
         public int Sqft { get; set; }
+        [Range(1, 100)]
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreateDate { get; set; }
         public DateTime? UpdateDate { get; set; }
