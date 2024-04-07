1dc451c8cdb47ee77a50f03709459a3179e9132a
Mohamedzonkol <mo.zonkol@gmail.com>
2024-04-07 02:59:07 +0200

    And Some Crud OPreation of Villa Model

---
+++ HotelWeb/Controllers/VillaController.cs
@@ -0,0 +1,34 @@
+﻿using Hotel.Domain.Entities;
+using Hotel.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelWeb.Controllers
+{
+    public class VillaController(ApplicationDbContext context) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(context.Villas.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+                ModelState.AddModelError("", "The Description Can Not Match The Name");
+
+            if (ModelState.IsValid)
+            {
+                context.Villas.Add(villa);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+
+        }
+    }
+}
